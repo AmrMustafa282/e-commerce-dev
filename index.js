eb27a5987e8587b497eb20228f0491bb47d42169
AmrMustafa282 <amrdarwish115@gmail.com>
2024-04-30 03:28:53 +0300

    init deploy-8

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ import { fileURLToPath } from "url";
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 const app = express();
 app.use(cors());
