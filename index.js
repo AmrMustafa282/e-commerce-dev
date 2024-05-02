821208d5469eeb1269034a0be0c6d6d2e755ae93
AmrMustafa282 <amrdarwish115@gmail.com>
2024-05-02 11:10:35 +0300

    fix webhook-4

--- index.js
+++ index.js
@@ -45,18 +45,8 @@ app.use(
   }
 
   // Handle the event
-  switch (event.type) {
-   case "payment_intent.succeeded":
-    const paymentIntent = event.data.object;
-    console.log("PaymentIntent was successful!");
-    break;
-   case "payment_method.attached":
-    const paymentMethod = event.data.object;
-    console.log("PaymentMethod was attached to a Customer!");
-    break;
-   // ... handle other event types
-   default:
-    console.log(`Unhandled event type ${event.type}`);
+  if (event.type === "payment_intent.succeeded") {
+   console.log("PaymentIntent was successful!");
   }
 
   // Return a response to acknowledge receipt of the event
